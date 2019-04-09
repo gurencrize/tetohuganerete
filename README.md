8607f849a28ca4e6f2c5a266becdcec4eac19fb2
gurencrize <34015092+gurencrize@users.noreply.github.com>
2019-04-09 11:08:08 +0900

    いろいろ修正
    テト譜を1行上にせりあげた
    色の判定方法を変えた
    pngのアルファチャンネルを破棄するようにした

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tetohuganerete
-http://crize.blog.jp/archives/16752321.html
+http://crize.blog.jp/archives/16752321.html  
+  
+2019/04/09
+せり上がりに使うラインにミノを置いていたのを修正しました   
+エフェクトの誤認を減らしました(たぶん…)  
+アルファチャンネル付きのpngを読み込めるようにしました
--- tetohu.py
+++ tetohu.py
@@ -1,16 +1,19 @@
 from PIL import Image,ImageDraw
 #テト譜の仕様→https://docs.google.com/presentation/d/1V4PNyt41to81phK9u0iXnIkAfp-nV3g3xcl7c5qW-FI/edit
 img = Image.open('original.png')
+img = img.convert("RGB")
 width, height = img.size
 draw = ImageDraw.Draw(img)
 square_width = int(width/10)
 square_height = int(height/20)
 URL = 'http://fumen.zui.jp/?v115@'
 tetohuconvert = [['A','B','C','D','E','F','G','H','I','J'],['K','L','M','N','O','P','Q','R','S','T'],['U','V','W','X','Y','Z','a','b','c','d'],['e','f','g','h','i','j','k','l','m','n'],['o','p','q','r','s','t','u','v','w','x'],['y','z','0','1','2','3','4','5','6','7'],['8','9','+','/']]
-tetohu = [8,39]
-def maketetohu1(r,g,b):
-    tetohulen=len(tetohu)
-    if r+b+g<200:
+tetohu = [8,29]
+def maketetohu(r,g,b):
+    tetohulen = len(tetohu)
+    colorlist = [r,g,b]
+    colorlist.sort()
+    if colorlist[2]-30<colorlist[1] and colorlist[1]-30<colorlist[0]:
         if tetohu[tetohulen-2] == 8:
             tetohu[tetohulen-1] += 1
         else:
@@ -26,7 +29,7 @@ def maketetohu1(r,g,b):
 for i in range(20):
     for j in range(10):
         r,g,b = img.getpixel((j*square_width+int(square_height/2),i*square_height+int(square_height/2)))
-        maketetohu1(r,g,b)
+        maketetohu(r,g,b)
         draw.point([j*square_width+int(square_height/2),i*square_height+int(square_height/2)],fill=(255,0,0))
 img.save('original_grided.png','PNG')
 while len(tetohu)>1:
